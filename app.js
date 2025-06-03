c29cb497aee9f2f2bfe27befbce576d3568d8988
= <=>
2025-06-03 13:56:32 +0200

    Add index page and router

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
+require('dotenv').config();
+const path = require('node:path');
 const express = require('express');
+const indexRouter = require('./routes/indexRouter');
+
 const app = express();
+const assetsPath = path.join(__dirname, 'public');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(assetsPath));
+
+app.use('/', indexRouter);
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
---
+++ routes/indexRouter.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+const indexRouter = Router();
+
+const messages = [
+    {
+        text: 'Hi there!', 
+        user: 'Amando',
+        added: new Date()
+    },
+    {
+        text: 'Hi everyone!', 
+        user: 'Billy',
+        added: new Date()
+    },
+];
+
+indexRouter.get('/', (req, res) => res.render('index', { title: "Mini Messageboard", messages }));
+
+module.exports = indexRouter;
