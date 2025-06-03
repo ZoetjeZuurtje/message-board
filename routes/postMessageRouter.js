1724e1ce60373d7f1a207938dc0df485eb0c6c96
= <=>
2025-06-03 15:12:38 +0200

    Refactor view files

--- routes/postMessageRouter.js
+++ routes/postMessageRouter.js
@@ -3,6 +3,6 @@
 const { Router } = require('express');
 const postMessageRouter = Router();
 
-postMessageRouter.get('/', (req, res) => res.render('form'));
+postMessageRouter.get('/', (req, res) => res.render('form', { title: 'Mini Messageboard | Create new post' }));
 
 module.exports = postMessageRouter;
